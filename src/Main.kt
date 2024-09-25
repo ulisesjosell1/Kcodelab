5ded07f4a188f1a07be0ac41e1b208244c8c02dd
ulisesjosell1 <ulisesjosell.gonzalezlopez@umg.edu.mx>
2024-09-25 11:03:45 -0600

    HOLA ULISES

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    fun printHello() {
+        println("Hello World")
+    }
+
+    printHello()
+}
